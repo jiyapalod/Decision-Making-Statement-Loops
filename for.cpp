4b9302daf126e3e0349fb142bf764667f9521d28
Jiya P <jiyapalod@gmail.com>
2024-08-10 20:45:51 +0530

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,20 @@
+//Jiya Palod
+//23070123070
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        cout<<"SIT"<<endl;
+    }
+}
+
+/* OUTPUT:
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
