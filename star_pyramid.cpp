7c63d139b8bd4b3886512ff2da9e61faddc49787
Jiya P <jiyapalod@gmail.com>
2024-08-10 20:51:51 +0530

    Add files via upload

---
+++ star_pyramid.cpp
@@ -0,0 +1,38 @@
+//Jiya Palod
+//23070123070
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j,k;
+    cout<<"Enter your choice: ";
+    cin>>n;
+    k=n;
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j>=k)
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        k--;
+        cout<<"\n";
+    }
+}
+
+/* OUTPUT:
+Enter your choice: 5
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+*/
